3df43712e8c76a199ea1dbbb3799cf2ac3df6a63
Jahidul Haque <jahidulpathan@gmail.com>
2019-03-24 19:38:09 +0600

    1st commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  
+  <form action="data.php" method="POST">
+    <div class="form-group">
+      <label for="url">Input URL:</label>
+      <input type="url" class="form-control" id="url" placeholder="Enter url" name="url" required>
+    </div>
+
+
+    
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form><br>
+
+
+ <p>
+        Example:
+
+        for # put this : https://www.amazon.in/gp/search/other/ref=sr_in_1_A?me=A368CFGDO8H247&rh=i%3Amerchant-items&pickerToList=brandtextbin&indexField=%23&ie=UTF8&qid=1552655543
+
+
+    </p>
+
+    <p>
+        Example:
+
+        for A put this : https://www.amazon.in/gp/search/other/ref=sr_in_a_1?me=A368CFGDO8H247&rh=i%3Amerchant-items&pickerToList=brandtextbin&indexField=a&ie=UTF8&qid=1552655548
+
+
+    </p>
+
+    <p>
+        Example:
+
+        for Top Brand put this : https://www.amazon.in/gp/search/other/ref=sr_in_-2_A?me=A368CFGDO8H247&rh=i%3Amerchant-items&pickerToList=brandtextbin&ie=UTF8&qid=1552572707
+
+
+    </p>
+
+    And so on...
+
+    and then wait for the download link to appear.
+
+</div>
+
+</body>
+</html>
